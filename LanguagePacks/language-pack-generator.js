f091e97819ad2d77d2cf5ae63d92255b4ba4d172
KoStard <rub099999720@gmail.com>
2018-09-01 11:12:22 +0400

    Added lowerTo, upperTo to the languagePackGenerator.

--- LanguagePacks/language-pack-generator.js
+++ LanguagePacks/language-pack-generator.js
@@ -7,16 +7,17 @@ const fs = require("fs");
 let data = JSON.parse(fs.readFileSync('./LanguagePacks/language-packs.json'));
 
 // Just call this function to add language pack and make colors better
-function generateLanguagePack({languageName, alphabetLength, lowerFrom, upperFrom}){
+// You can give lowerTo, upperTo or just leave them undefined
+function generateLanguagePack({languageName, alphabetLength, lowerFrom, lowerTo, upperFrom, upperTo}){
     if (!alphabetLength || !lowerFrom || !upperFrom) return false;
     data[languageName] = {
         length: alphabetLength,
         lower: {
             from: lowerFrom,
-            to: String.fromCharCode(lowerFrom.charCodeAt(0)+alphabetLength-1)
+            to: lowerTo || String.fromCharCode(lowerFrom.charCodeAt(0) + alphabetLength - 1)
         }, upper: {
             from: upperFrom,
-            to: String.fromCharCode(upperFrom.charCodeAt(0) + alphabetLength - 1)
+            to: upperTo || String.fromCharCode(upperFrom.charCodeAt(0) + alphabetLength - 1)
         },
     };
     saveLP();
